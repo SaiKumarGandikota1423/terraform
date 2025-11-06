e458e9b4f8704a131687a0478fdfbb891fcc6310
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-11-06 20:03:18 +0530

    added terrform repo

--- session-01/ec2/provider.tf
+++ session-01/ec2/provider.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.19.0"
-    }
- }
-}
+  terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "6.19.0"
+      }
+  }
+  }
 
-provider "aws" {
-    # Configuration options
-    ## Configure the AWS Provider 
-   region = "us-east-1"
-}
+  provider "aws" {
+      # Configuration options
+      ## Configure the AWS Provider 
+    region = "us-east-1"
+  }
