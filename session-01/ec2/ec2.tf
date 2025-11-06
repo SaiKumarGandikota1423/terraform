577d7aae803ca5080cd338761783d8c72e05df70
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-11-06 11:06:12 +0530

    added terrform repo

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
-  ami           = ami-0b4f379183e5706b9 #Centos-8-DevOps-Practice-AMI
-  instance_type = "t3.micro"
+  ami           = "ami-0b4f379183e5706b9" #Centos-8-DevOps-Practice-AMI
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloTerraform"
