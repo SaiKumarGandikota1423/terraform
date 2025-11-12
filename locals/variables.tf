f27c21f0cc2669d7a5a26ad4bef9261cec58bc82
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-11-12 09:09:26 +0530

    added new files in terraform repo

---
+++ locals/variables.tf
@@ -0,0 +1,17 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "zone_id" {
+  default = "Z03390721TAW3412ZTWXG"
+}
+
+variable "domain_name" {
+  default = "devopsawscloud.shop
+"
+}
